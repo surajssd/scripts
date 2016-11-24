62ab125283d14a162d92deb8ee9f75c76bd2882a
Suraj Deshmukh <surajssd009005@gmail.com>
2016-11-24 10:11:47 +0530

    pull url shortner

--- shell/post_machine_install/installer.sh
+++ shell/post_machine_install/installer.sh
@@ -46,29 +46,8 @@ curl -O https://raw.githubusercontent.com/surajssd/scripts/master/shell/configs/
 curl -O https://raw.githubusercontent.com/surajssd/scripts/master/shell/configs/.gitconfig
 
 #==================================================================
-mkdir -p ~/.local/bin
-cat > ~/.local/bin/shorturl <<EOF
-#!/bin/env python
-
-import sys
-import requests
-
-URL = 'http://api.bit.ly/v3/shorten'
-
-data = {
-    "login": "",
-    "apiKey": "",
-    "longURL": sys.argv[1],
-    "format": "txt"
-}
-
-response = requests.get(URL, data)
-print response.text
-EOF
-chmod a+x ~/.local/bin/shorturl
-echo ">>>>> Goto https://bitly.com/a/settings/advanced"
-echo ">>>>> and copy Login and API Key and put in "
-echo ">>>>> ~/.local/bin/shorturl "
+# url shortner
+curl -O https://raw.githubusercontent.com/surajssd/scripts/master/shell/post_machine_install/shorturl.sh
 
 #===================================================================
 # Install pathogen
