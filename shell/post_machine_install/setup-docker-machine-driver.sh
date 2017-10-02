5ed1d06e5661d54d26646497d18acb8004f90bc8
Suraj Deshmukh <surajd.service@gmail.com>
2017-10-03 00:18:34 +0530

    add setup docker machine driver script

---
+++ shell/post_machine_install/setup-docker-machine-driver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                    
+
+set -xe                                        
+
+which docker-machine-driver-kvm                
+if [ $? -ne 0 ]; then                          
+    sudo sh -c 'curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 > /usr/local/bin/docker-machine-driver-kvm                 
+    chmod +x /usr/local/bin/docker-machine-driver-kvm'                                         
+fi                                             
+
+sudo dnf install -y libvirt qemu-kvm           
+sudo usermod -aG libvirt $USER                 
+newgrp libvirt                               
+
