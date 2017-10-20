a1d5e80389245a005bd6099d54c28344682d43b9
Suraj Deshmukh <surajd.service@gmail.com>
2017-10-21 02:07:51 +0530

    auto download the latest golang

--- shell/installer_go/pullgo.sh
+++ shell/installer_go/pullgo.sh
@@ -2,15 +2,16 @@
 
 set -x
 
-VERSION=1.9.1
 
 # check if wget is installed
 which wget
 if [ $? -ne 0 ]; then sudo dnf -y install wget; fi
 
-mkdir -p ~/Downloads
-cd ~/Downloads
+mkdir -p /tmp/goinstall
+cd /tmp/goinstall
 
-wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
+wget -O /tmp/golangversion https://golang.org/dl/
+wget $(grep -r linux /tmp/golangversion  | grep amd64 | grep "storage.googleapis.com" | head -1 | grep -Eo 'https://[^ ">]+')
+
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go*
