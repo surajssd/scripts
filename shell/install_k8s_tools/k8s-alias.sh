6ecc40b219d315856b80dc8074d895ed19e49b47
Suraj Deshmukh <surajd.service@gmail.com>
2017-12-11 18:03:05 +0530

    add diffapply and k8s-alias

--- shell/install_k8s_tools/k8s-alias.sh
+++ shell/install_k8s_tools/k8s-alias.sh
@@ -8,13 +8,13 @@ echo '
 # k8s alias
 
 alias k=kubectl
-alias kg='kubectl get' 
-alias kgp='kubectl get pods'
-alias kgs='kubectl get services'
-alias kge='kubectl get events'
-alias kgpvc='kubectl get pvc'
-alias kgpv='kubectl get pv'
-alias kc='kubectl create -f '
+alias kg="'"kubectl get"'"
+alias kgp="'"kubectl get pods"'"
+alias kgs="'"kubectl get services"'"
+alias kge="'"kubectl get events"'"
+alias kgpvc="'"kubectl get pvc"'"
+alias kgpv="'"kubectl get pv"'"
+alias kc="'"kubectl create -f "'"
 
 function current-ns() {
 	kubectl get secret $(kubectl get sa default -o jsonpath="'"{.secrets[0].name}"'") -o jsonpath="'"{.data.namespace}"'" | base64 -d
---
+++ shell/post_machine_install/diffapply.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+url=$1
+if [ -z $url ]; then
+    echo "Please provide the URL!"
+    exit -1
+fi;
+
+RANDOM=$$
+rand=${RANDOM}
+curl -o /tmp/diff-${rand} ${url}raw
+
+git checkout .
+git apply /tmp/diff-${rand}
+
