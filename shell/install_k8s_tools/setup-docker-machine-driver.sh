fdd863f5c95be8f767899612cdea1f46337b652e
Suraj Deshmukh <surajd.service@gmail.com>
2017-12-20 10:31:06 +0530

    install the kvm driver

--- shell/install_k8s_tools/setup-docker-machine-driver.sh
+++ shell/install_k8s_tools/setup-docker-machine-driver.sh
@@ -1,14 +1,13 @@
-#!/bin/bash                                    
+#!/bin/bash
 
-set -x                                        
+set -x
 
-which docker-machine-driver-kvm                
-if [ $? -ne 0 ]; then                          
-    sudo sh -c 'curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 > /usr/local/bin/docker-machine-driver-kvm                 
-    chmod +x /usr/local/bin/docker-machine-driver-kvm'                                         
-fi                                             
+which docker-machine-driver-kvm
+if [ $? -ne 0 ]; then
+    sudo sh -c 'curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 > /usr/local/bin/docker-machine-driver-kvm
+    chmod +x /usr/local/bin/docker-machine-driver-kvm'
+fi
 
-sudo dnf install -y libvirt qemu-kvm           
-sudo usermod -aG libvirt $USER                 
-newgrp libvirt                               
-
+sudo dnf install -y libvirt qemu-kvm
+sudo usermod -aG libvirt $USER
+newgrp libvirt
