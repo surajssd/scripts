36b333e1b5416863051945f04ed575f51842ba75
Suraj Deshmukh <surajd.service@gmail.com>
2018-05-08 10:45:48 +0530

    update the go instal

--- shell/installer_go/pullgo.sh
+++ shell/installer_go/pullgo.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-set -x
+if [ -z $version ]; then
+    echo "Please provide the go version, by exporting variable 'version',
+    export version=\"go1.10.2\""
+    exit 1
+fi
 
-sudo dnf -y install git
+# check if git is installed, if not install it
+if ![ -x "$(command -v git)" ]; then
+    echo "Installing git..."
+    sudo dnf -y install git
+fi
+
+
 # check if wget is installed
-which wget
-if [ $? -ne 0 ]; then sudo dnf -y install wget; fi
+if ![ -x "$(command -v wget)" ]; then
+    echo "Installing wget..."
+    sudo dnf -y install wget
+fi
 
 mkdir -p /tmp/goinstall
 cd /tmp/goinstall
 
-wget -O /tmp/golangversion https://golang.org/dl/
-wget $(grep -r linux /tmp/golangversion  | grep amd64 | grep "redirector.gvt1.com" | head -1 | grep -Eo 'https://[^ ">]+')
+url=https://dl.google.com/go/$version.linux-amd64.tar.gz
 
+echo "Downloading from $url"
+wget $url
+if [ $? -ne 0 ]; then
+    echo "Go downloading failed"
+    exit 1
+fi
+
+echo "Uninstalling $(go version)"
 sudo rm -rf /usr/local/go
+
+echo "Installing new $version"
 sudo tar -C /usr/local -xzf go*
 sudo ln -s /usr/local/go/bin/go /usr/local/sbin/go
