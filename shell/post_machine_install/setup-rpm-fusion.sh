7a496cc471818895824f66182e0bcdf1a6d11fe6
Suraj Deshmukh <surajd.service@gmail.com>
2018-09-08 17:16:00 +0530

    update the rpm fusion

--- shell/post_machine_install/setup-rpm-fusion.sh
+++ shell/post_machine_install/setup-rpm-fusion.sh
@@ -3,4 +3,4 @@
 set -x
 
 # rpm installead softwares
-su -c 'dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
+sudo dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
