a884859ee8e1d6b4cd455d8507859f168c9ced36
Suraj Deshmukh <surajd.service@gmail.com>
2019-06-04 14:38:47 +0530

    upgrade the bit to 8192
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- shell/post_machine_install/ssh-key-gen.sh
+++ shell/post_machine_install/ssh-key-gen.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Generate new key
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "" -C "surajd.service@gmail.com"
+ssh-keygen -t rsa -b 8192 -f ~/.ssh/id_rsa -N "" -C "surajd.service@gmail.com"
 
 echo "Add the key in ~/.ssh/id_rsa.pub to https://github.com/settings/keys"
 echo "After that run -> ssh -T git@github.com"
